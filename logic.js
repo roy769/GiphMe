8845a4262e01fdcba0d73d9636676898239a1fdd
roy769 <roy@mortgallery.com>
2017-11-02 18:03:56 -0700

    nick at nite

---
+++ logic.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  var Nick = [  "doug", "rugrats", "pete and pete", "ren and stimpy" "hey arnold", "rockos modern life", "you cant do that on television", "all that"
+];
+
+function populateButtons(arrayToUse) {
+
+}
+})
