1e1b6b0c38ae14d6cfcdd2c050330e626a38b091
roy769 <roy@mortgallery.com>
2017-11-07 19:56:01 -0800

    gettin it

--- logic.js
+++ logic.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 
+
   var Nick = [  "doug", "rugrats", "pete and pete", "ren and stimpy" "hey arnold", "rockos modern life", "you cant do that on television", "all that"
 ];
 
-function populateButtons(arrayToUse) {
+function populateButtons(arrayToUse, classToAdd, areaToAdd) {
+  $(areaToAddTo).empty();
 
+  for (var i = 0; i < arrayToUse.length; i++) {
+    var nick = $("<button>");
+    nick.addClass(classToAdd);
+    nick.attr("data-type", arrayToUse[i]);
+    nick.text(arrayToUse[i]);
+    $(areaToAddTo).append(a);
+  }
 }
 })
